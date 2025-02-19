47409462563214e3c7b0b006ede6baf3db4c0ffb
XToodyX <dherrmann.oas@gmail.com>
2025-02-19 16:30:32 +0100

    refactor color selection and clear canvas functionality; add visual feedback for selected color

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const canvasElement = document.querySelector('#draw');
-  const ctx = canvasElement.getContext('2d');
+const ctx = canvasElement.getContext('2d');
 
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 5;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 5;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
 
-  let isDrawing = false;
-  let lastX = 0;
-  let lastY = 0;
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
 
-  function draw(e) {
-    if (!isDrawing) return;
+function draw(e) {
+  if (!isDrawing) return;
 
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvasElement.width, canvasElement.height)
+}
+
+function setColor(color, selectedButton) {
+  ctx.strokeStyle = color;
+
+  const previousSelectedButton = document.querySelector('.color-buttons .selected');
+
+  if (previousSelectedButton) {
+    previousSelectedButton.classList.remove('selected')
   }
+  selectedButton.classList.add('selected');  
+}
 
-  canvasElement.addEventListener('mousedown', (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-  });
+canvasElement.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 
-  canvasElement.addEventListener('mouseup', () => isDrawing = false);
-  canvasElement.addEventListener('mouseout', () => isDrawing = false);
-  canvasElement.addEventListener('mousemove', draw);
+canvasElement.addEventListener('mouseup', () => isDrawing = false);
+canvasElement.addEventListener('mouseout', () => isDrawing = false);
+canvasElement.addEventListener('mousemove', draw);
